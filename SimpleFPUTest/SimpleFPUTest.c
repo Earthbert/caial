96eb41a4a677946012870577813cd966c8b724ef
Earthbert <daraban_timotei@tutanota.com>
2025-05-08 13:43:39 +0300

    update SimpleFPUTest
    Signed-off-by: Earthbert <daraban_timotei@tutanota.com>

--- SimpleFPUTest/SimpleFPUTest.c
+++ SimpleFPUTest/SimpleFPUTest.c
@@ -1,8 +1,19 @@
+#include <stdio.h>
+
 int main() {
-    float a = 3.0f;
-    float b = 6.0f;
+  float a = 1.0f;
+  float b = 2.0f;
+  float c = 3.0f;
 
-    int result = a + b;
+  float result = 0.0f;
 
-    return result;
+  asm volatile (
+    "fmadd.s %0, %1, %2, %3"
+    : "=f"(result)
+    : "f"(a), "f"(b), "f"(c)
+  );
+
+  printf("Hex Result: %x\n", *(unsigned int *)&result);
+
+  return 0;
 }
