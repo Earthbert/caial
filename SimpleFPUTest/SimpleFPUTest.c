d3ffd1527ddbb89babb1879a250db4401d1125f3
Earthbert <daraban_timotei@tutanota.com>
2025-06-08 15:56:00 +0300

    Add double tests
    Signed-off-by: Earthbert <daraban_timotei@tutanota.com>

--- SimpleFPUTest/SimpleFPUTest.c
+++ SimpleFPUTest/SimpleFPUTest.c
@@ -1,7 +1,7 @@
 #include "data.h"
 #include <stdio.h>
 
-#define NRS 1 // 0 for IEEE754, 1 for POSIT
+#define NRS 0 // 0 for IEEE754, 1 for POSIT
 
 void check_float_result(float res, unsigned int expected, const char *operation) {
   unsigned int res_int = *(long *)&res;
@@ -12,11 +12,28 @@ void check_float_result(float res, unsigned int expected, const char *operation)
   }
 }
 
+void check_double_result(double res, unsigned long long expected, const char *operation) {
+  unsigned long long res_int = *(unsigned long long *)&res;
+  if (res_int != expected) {
+    printf("Error in %s: expected 0x%llx, got 0x%llx\n", operation, expected, res_int);
+  } else {
+    printf("Success in %s: got 0x%llx\n", operation, res_int);
+  }
+}
+
 void check_int_result(unsigned int res, unsigned int expected, const char *operation) {
   if (res != expected) {
     printf("Error in %s: expected 0x%x, got 0x%x\n", operation, expected, res);
   } else {
     printf("Success in %s: got 0x%x\n", operation, res);
+  }
+}
+
+void check_long_result(unsigned long long res, unsigned long long expected, const char *operation) {
+  if (res != expected) {
+    printf("Error in %s: expected 0x%llx, got 0x%llx\n", operation, expected, res);
+  } else {
+    printf("Success in %s: got 0x%llx\n", operation, res);
   }
 }
 
@@ -66,4 +83,53 @@ void test_float_operations() {
   }
 }
 
-int main() { test_float_operations(); }
+void test_double_operations() {
+  printf("Testing double operations...\n");
+
+  for (int i = 0; i < sizeof(double_inputs) / sizeof(double_inputs[0]); i++) {
+    double a = double_inputs[i][0];
+    double b = double_inputs[i][1];
+    double c = double_inputs[i][2];
+
+    double d_res;
+    asm volatile("fmadd.d %0, %1, %2, %3" : "=f"(d_res) : "f"(a), "f"(b), "f"(c));
+    check_double_result(d_res, expected_double_results[i][0][NRS], "fmadd");
+    asm volatile("fmsub.d %0, %1, %2, %3" : "=f"(d_res) : "f"(a), "f"(b), "f"(c));
+    check_double_result(d_res, expected_double_results[i][1][NRS], "fmsub");
+    asm volatile("fnmadd.d %0, %1, %2, %3" : "=f"(d_res) : "f"(a), "f"(b), "f"(c));
+    check_double_result(d_res, expected_double_results[i][2][NRS], "fnmadd");
+    asm volatile("fnmsub.d %0, %1, %2, %3" : "=f"(d_res) : "f"(a), "f"(b), "f"(c));
+    check_double_result(d_res, expected_double_results[i][3][NRS], "fnmsub");
+    asm volatile("fadd.d %0, %1, %2" : "=f"(d_res) : "f"(a), "f"(b));
+    check_double_result(d_res, expected_double_results[i][4][NRS], "fadd");
+    asm volatile("fsub.d %0, %1, %2" : "=f"(d_res) : "f"(a), "f"(b));
+    check_double_result(d_res, expected_double_results[i][5][NRS], "fsub");
+    asm volatile("fmul.d %0, %1, %2" : "=f"(d_res) : "f"(a), "f"(b));
+    check_double_result(d_res, expected_double_results[i][6][NRS], "fmul");
+
+    unsigned int i_res;
+    asm volatile("feq.d %0, %1, %2" : "=r"(i_res) : "f"(a), "f"(b));
+    check_long_result(i_res, expected_double_results[i][7][NRS], "feq");
+    asm volatile("flt.d %0, %1, %2" : "=r"(i_res) : "f"(a), "f"(b));
+    check_long_result(i_res, expected_double_results[i][8][NRS], "flt");
+    asm volatile("fle.d %0, %1, %2" : "=r"(i_res) : "f"(a), "f"(b));
+    check_long_result(i_res, expected_double_results[i][9][NRS], "fle");
+    asm volatile("fabs.d %0, %1" : "=f"(d_res) : "f"(a));
+    check_double_result(d_res, expected_double_results[i][10][NRS], "fabs");
+    asm volatile("fneg.d %0, %1" : "=f"(d_res) : "f"(a));
+    check_double_result(d_res, expected_double_results[i][11][NRS], "fneg");
+    asm volatile("fmax.d %0, %1, %2" : "=f"(d_res) : "f"(a), "f"(b));
+    check_double_result(d_res, expected_double_results[i][12][NRS], "fmax");
+    asm volatile("fmin.d %0, %1, %2" : "=f"(d_res) : "f"(a), "f"(b));
+    check_double_result(d_res, expected_double_results[i][13][NRS], "fmin");
+    asm volatile("fdiv.d %0, %1, %2" : "=f"(d_res) : "f"(a), "f"(b));
+    check_double_result(d_res, expected_double_results[i][14][NRS], "fdiv");
+    asm volatile("fsqrt.d %0, %1" : "=f"(d_res) : "f"(a));
+    check_double_result(d_res, expected_double_results[i][15][NRS], "fsqrt");
+  }
+}
+
+int main() {
+  // test_float_operations();
+  test_double_operations();
+}
